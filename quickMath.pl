cea169a915044d158e5d6ed669ff65a9ce239a5b
Exp <theexp111@gmail.com>
2018-04-21 11:53:10 +0200

    Fucking math

--- quickMath.pl
+++ quickMath.pl
@@ -1,12 +1,17 @@
 % Autor:
 % Datum: 21.04.2018
 
-plus(o,X,X).
-plus(s(A), B, s(R)) :- plus(A,B,R).
+plus(o,X,X) :- natSymb(X).
+plus(s(A), B, s(R)) :- natSymb(A), natSymb(B), plus(A,B,R).
 
-minus(A,B,R) :- plus(A,B,R), minus.
+minus(A,B,R) :- plus(B,R,A).
 
-mult(o,A,o).
-mult(A,s(B),R) :- mult(A,B,U), plus(A,U,R).
+mult(A,o,o) :- natSymb(A).
+mult(A,s(B),R) :- natSymb(A), natSymb(B), mult(A,B,U), plus(A,U,R).
 
 eqZero(0).
+
+square(X,Y) :- natSymb(X), natSymb(Y), mult(X,X,Y).
+
+natSymb(o).
+natSymb(s(X)) :- natSymb(X).
