01bdc0a96b95b2d3e96b1edbe03bec6230e93aae
T N <theexp111@gmail.com>
2018-04-26 08:22:52 +0200

    exp function
    haha xd kappa lol roflcopter

--- quickMath.pl
+++ quickMath.pl
@@ -5,6 +5,7 @@ plus(o,X,X) :- natSymb(X).
 plus(s(A), B, s(R)) :- natSymb(A), natSymb(B), plus(A,B,R).
 
 minus(A,B,R) :- plus(B,R,A).
+halbieren(A, R) :- plus(R, R, A).
 
 mult(A,o,o) :- natSymb(A).
 mult(A,s(B),R) :- natSymb(A), natSymb(B), mult(A,B,U), plus(A,U,R).
@@ -17,3 +18,7 @@ square(X,Y) :- natSymb(X), natSymb(Y), mult(X,X,Y).
 
 natSymb(o).
 natSymb(s(X)) :- natSymb(X).
+
+%exp(A, s(B), R) :- A^B = R
+exp(A, o, s(o)).
+exp(A, s(B), R) :- natSymb(A), natSymb(B), exp(A, B, U), mult(A,U,R).
