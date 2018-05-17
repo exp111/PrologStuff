d9035604ff37d3984af444ac7626d6b849a6a92b
Exp <theexp111@gmail.com>
2018-05-17 13:46:33 +0200

    PERMUTATION

--- Ubung4.pl
+++ Ubung4.pl
@@ -1,7 +1,7 @@
 % Autor:
 % Datum: 13.05.2018
 
-%append(Xs,Ys,Rs) :- Dabei ergibt sich die Liste Rs durch Anhängen der Liste Ys an die Liste Xs.
+%append(Xs,Ys,Rs) :- Dabei ergibt sich die Liste Rs durch AnhÃ¤ngen der Liste Ys an die Liste Xs.
 app([], Ys, Ys).
 app([H|Xs], Ys, [H|Rs]) :- app(Xs, Ys, Rs).
 
@@ -17,15 +17,15 @@ maxSymb(s(A),s(B),s(R)) :- maxSymb(A,B,R).
 
 
 %A25)
-%tiefe(Xb, Ys) :- Ys ist die Liste der Knotenbeschriftungen des Binärbaumes Xb,
-%die man bei einem depth-first left-to-right Durchlauf erhält.
+%tiefe(Xb, Ys) :- Ys ist die Liste der Knotenbeschriftungen des BinÃ¤rbaumes Xb,
+%die man bei einem depth-first left-to-right Durchlauf erhÃ¤lt.
 %n(0, n(1, n(2, e, e), n(3,e,e)), n(4, e, n(5, e, e)))
 tiefe(e, []).
 tiefe(n(H, Lb, Rb),[H|Ys]) :- tiefe(Lb, Ys1), tiefe(Rb, Ys2), app(Ys1, Ys2, Ys).
 
 
 %A27)
-%height(Tb, N) :- N (nat. Zahl in symbolischer Darstellung) ist die Höhe des Baumes (siehe Aufgabe 7.c) T.
+%height(Tb, N) :- N (nat. Zahl in symbolischer Darstellung) ist die HÃ¶he des Baumes (siehe Aufgabe 7.c) T.
 height(e, o).
 height(n(H, Lb, Rb), s(N)) :- height(Lb, N1), height(Rb, N2), maxSymb(N1, N2, N).
 
@@ -43,28 +43,32 @@ varcons(n(Root, Lb, Rb), s(N)) :- varcons(Lb, N1), varcons(Rb, N2), add(N1, N2, 
 selectL(X, [X|Xs], Xs).
 selectL(X, [H|Xs], [H|Ys]) :- selectL(X, Xs, Ys).
 
-%insert(X, Xs, Ys) :- Die Listen Ys ergeben sich aus der Liste Xs durch einmaliges Einfügen von X an beliebiger Stelle.
+%insert(X, Xs, Ys) :- Die Listen Ys ergeben sich aus der Liste Xs durch einmaliges EinfÃ¼gen von X an beliebiger Stelle.
 insert(X, Xs, [X|Xs]).
 insert(X, [H|Xs], [H|Ys]) :- insert(X, Xs, Ys).
+%insert(X, Xs, YS) :- selectL(X,Ys,Xs).
 
-%permutationL(Xs, Ys) :- Die Listen Ys sind jeweils eine Permutation der Liste Xs.
-permutationL([H|Xs], Ys) :- insert(H, Xs, Ys).
-permutationL([H|Xs], [H|Ys]) :- permutationL(Xs, Ys).
+%permutation(Xs, Ys) :- Die Listen Ys sind jeweils eine Permutation der Liste Xs.
+perm1([],[]).
+
+
+perm2([],[]).
+perm2([X|Xs],Ys) :- perm2(Xs, Rs), insert(X,Rs,Ys).
 
 
 %A29)
-%invListNum(list(s(H), list(H, T))) :- list enthält die ersten natürlichen Zahlen in numerischer Darstellung in umgekehrter Reihenfolge aufsteigend
+%invListNum(list(s(H), list(H, T))) :- list enthÃ¤lt die ersten natÃ¼rlichen Zahlen in numerischer Darstellung in umgekehrter Reihenfolge aufsteigend
 invListNum([]).
 invListNum([0]).
 invListNum([H1|[H2|Ts]]) :- invListNum([H2|Ts]), H1 is H2 + 1.
 
 
 %A30)
-%listLength(Xs,N) :- Die natürliche Zahl N ist die Länge der Liste Xs in numerischer Darstellung.
+%listLength(Xs,N) :- Die natÃ¼rliche Zahl N ist die LÃ¤nge der Liste Xs in numerischer Darstellung.
 listLength([],0).
 listLength([X|Xs],N) :- listLength(Xs, N1), N is N1 + 1.
 
-%anz(X,Xs,N) :- Die natürliche Zahl N ist die Häufigkeit des Auftretens von X in Xs in numerischer Darstellung.
+%anz(X,Xs,N) :- Die natÃ¼rliche Zahl N ist die HÃ¤ufigkeit des Auftretens von X in Xs in numerischer Darstellung.
 anz(X, [], 0).
 anz(X,[X|Xs],N) :- anz(X, Xs, N1), N is N1 + 1.
 anz(X,[H|Xs],N) :- X =\= H, anz(X, Xs, N).
