d5014fa7899de49fa8e97a65b4b6a5101ebd8b6e
Exp <theexp111@gmail.com>
2018-05-17 22:23:27 +0200

    it works (but crashes if false?)

--- Praktikum2.pl
+++ Praktikum2.pl
@@ -9,13 +9,18 @@ linListe(list(X, Xs)) :- linListe(Xs).
 
 %b)
 %memberS(X, Ys) :- Ys enthält das Element X.
-memberS(X, list(X, Ys)).
-memberS(X, list(Y, Ys)) :- memberS(X, Ys).
+memberS(X, list(X, Ys)) :- linListe(Ys).
+memberS(X, list(Y, Ys)) :- linListe(Ys), memberS(X, Ys).
+
+%append(Xs,Ys,Rs) :- Dabei ergibt sich die Liste Rs durch Anhängen der Liste Ys an die Liste Xs.
+app(nil, Ys, Ys).
+app(list(H, Xs), Ys, list(H, Rs)) :- app(Xs, Ys, Rs).
 
 %infix(Xs, Ys) :- Ys enthält die Liste Xs
-infix(nil, Ys).
-infix(list(X, Xs), list(X, Ys)) :- infix(Xs, Ys).
-infix(Xs, list(Y, Ys)) :- infix(Xs, Ys).
+infix(nil, Ys) :- linListe(Ys).
+infix(list(X, Xs), list(X, Ys)) :- linListe(Xs), linListe(Ys), infix(Xs, Ys).
+infix(Xs, list(Y, Ys)) :- linListe(Xs), linListe(Ys), infix(Xs, Ys).
+inf(Xs, Ys) :- linListe(Xs), linListe(Ys), app(M1s, Xs, M2s), app(M2s, M3s, Ys).
 
 %A2
 %n(s(s(o)), n(o, e, n(s(s(s(o))), e, e)), n(s(o), n(s(s(s(s(o)))), e, e))).
