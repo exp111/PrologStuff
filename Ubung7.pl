ce242aa9ec6ac370b4990bd8f6d7a492e6638c84
T N <theexp111@gmail.com>
2018-06-03 14:23:51 +0200

    übung 7

---
+++ Ubung7.pl
@@ -0,0 +1,12 @@
+% Autor:
+% Datum: 03.06.2018
+
+%A16.)
+%mirror(Xb,Yb) :- Yb ist der gespiegelte Baum Xb
+mirror(e, e).
+mirror(n(R,Lb,Rb), n(R, RMb, LMb)) :- mirror(Lb, LMb), mirror(Rb, RMb).
+
+%A31.) a)
+%removeLastElement(Xs, Ys) :- Ys ist Xs, eine nicht leere Liste in Prolog Listennotation, ohne das letzte Element.
+removeLastElement([_],[]).
+removeLastElement([X|Xs],[X|Ys]) :- removeLastElement(Xs,Ys).
